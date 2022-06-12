c40440406d80e2075c61de2635a245288c3cfdf5
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-13 07:47:57 +1000

    🎉 Update

--- src/api/manufacturer-type/controllers/manufacturer-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  manufacturer-type controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::manufacturer-type.manufacturer-type');
--- src/api/manufacturer-type/services/manufacturer-type.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * manufacturer-type service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::manufacturer-type.manufacturer-type');
