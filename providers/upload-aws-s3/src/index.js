fbc97ea95e98dd0ef754678297e231ee4d8c5ce9
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-06-18 07:46:57 +1000

    🎉 Update

---
+++ providers/upload-aws-s3/src/index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  init(providerOptions) {
+    // init your provider if necessary
+
+    return {
+      upload(file) {
+        // upload the file in the provider
+        // file content is accessible by `file.buffer`
+        console.log(file);
+      },
+      uploadStream(file) {
+        // upload the file in the provider
+        // file content is accessible by `file.stream`
+      },
+      delete(file) {
+        // delete the file in the provider
+      },
+    };
+  },
+};
