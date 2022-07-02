eb31c09f95255de7d007caa3b2c080515dfcc383
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-02 18:47:56 +1000

    🎉 Update

---
+++ src/api/manufacturer-account/controllers/manufacturer-account.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  manufacturer-account controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::manufacturer-account.manufacturer-account');
---
+++ src/api/manufacturer-account/services/manufacturer-account.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * manufacturer-account service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::manufacturer-account.manufacturer-account');
