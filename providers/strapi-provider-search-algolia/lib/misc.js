fa1c85615eab83139834d42fd7f860f88dbc445b
BenGOsborn <bengrantalbertosborn@gmail.com>
2022-07-08 10:32:32 +1000

    🎉 Update

--- providers/strapi-provider-search-algolia/lib/misc.js
+++ providers/strapi-provider-search-algolia/lib/misc.js
@@ -2,6 +2,6 @@ module.exports.parseLocation = function parseLocation(data) {
     if (!data.location) return {};
 
     return {
-        _geoloc: data.location.map((entry) => ({ lat: entry.latitude, lng: entry.longitude })),
+        _geoloc: data.location.length > 0 ? data.location.map((entry) => ({ lat: entry.latitude, lng: entry.longitude })) : [{ lat: -22.9778201, lng: 132.5032925 }],
     };
 };
